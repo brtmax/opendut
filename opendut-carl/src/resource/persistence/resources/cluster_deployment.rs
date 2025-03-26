dd6a4d909617c1df9cfe6b7171a418fec6463095
Frank Märkle <Frank.Maerkle@mercedes-benz.com>
2025-03-26 15:19:14 +0100

    WIP CARL Resources -> Replace diesel with redb. Test `resource::storage::tests::cluster_deployment::should_persist_cluster_deployment_in_database` works.

--- opendut-carl/src/resource/persistence/resources/cluster_deployment.rs
+++ opendut-carl/src/resource/persistence/resources/cluster_deployment.rs
@@ -1,6 +1,7 @@
 use opendut_types::cluster::{ClusterDeployment, ClusterId};
 use std::collections::HashMap;
-
+use redb::{ReadableTable, TableDefinition};
+use uuid::Uuid;
 use crate::resource::persistence::error::PersistenceResult;
 use crate::resource::persistence::query::Filter;
 use crate::resource::persistence::{query, Storage, Storage2};
@@ -27,19 +28,86 @@ impl Persistable for ClusterDeployment {
         query::cluster_deployment::list(Filter::Not, &mut storage.db.connection())
     }
 
-    fn insert2(self, id: Self::Id, storage: &mut Storage2) -> PersistenceResult<()> {
-        todo!()
+
+    fn insert2(self, cluster_id: ClusterId, storage: &mut Storage2) -> PersistenceResult<()> {
+        let mut table = storage.db.open_table(CLUSTER_DEPLOYMENT_TABLE).unwrap(); //TODO don't unwrap
+
+        let key = cluster_id.0.as_bytes().as_slice();
+
+        // let value = { //TODO
+        //     let PeerDescriptor { id, name, location, network, topology, executors } = self;
+        //     SerializablePeerDescriptor {
+        //         id, name, location, network, topology, executors,
+        //     }
+        // };
+        let value = self;
+        let value = serde_json::to_string(&value).unwrap(); //TODO don't unwrap
+
+        table.insert(key, value).unwrap(); //TODO don't unwrap
+
+        Ok(())
     }
 
-    fn remove2(id: Self::Id, storage: &mut Storage2) -> PersistenceResult<Option<Self>> {
-        todo!()
+    fn remove2(cluster_id: ClusterId, storage: &mut Storage2) -> PersistenceResult<Option<Self>> {
+        let mut table = storage.db.open_table(CLUSTER_DEPLOYMENT_TABLE).unwrap(); //TODO don't unwrap
+
+        let key = cluster_id.0.as_bytes().as_slice();
+
+        let value = table.remove(key).unwrap() //TODO don't unwrap
+            .map(|value| {
+                let value = serde_json::from_str::<ClusterDeployment>(&value.value()).unwrap(); //TODO don't unwrap
+
+                // let value = {
+                //     let SerializablePeerDescriptor { id, name, location, network, topology, executors } = peer_descriptor;
+                //     PeerDescriptor { id, name, location, network, topology, executors }
+                // };
+                value
+            });
+
+        Ok(value)
     }
 
-    fn get2(id: Self::Id, storage: &Storage2) -> PersistenceResult<Option<Self>> {
-        todo!()
+    fn get2(cluster_id: ClusterId, storage: &Storage2) -> PersistenceResult<Option<Self>> {
+        let table = storage.db.open_table(CLUSTER_DEPLOYMENT_TABLE).unwrap(); //TODO don't unwrap
+
+        let key = cluster_id.0.as_bytes().as_slice();
+
+        let value = table.get(key).unwrap() //TODO don't unwrap
+            .map(|value| {
+                let value = serde_json::from_str::<ClusterDeployment>(&value.value()).unwrap(); //TODO don't unwrap
+
+                // let value = {
+                //     let SerializablePeerDescriptor { id, name, location, network, topology, executors } = value;
+                //     PeerDescriptor { id, name, location, network, topology, executors }
+                // };
+                value
+            });
+
+        Ok(value)
     }
 
     fn list2(storage: &Storage2) -> PersistenceResult<HashMap<Self::Id, Self>> {
-        todo!()
+        let table = storage.db.open_table(CLUSTER_DEPLOYMENT_TABLE).unwrap(); //TODO don't unwrap
+
+        let value = table.iter().unwrap() //TODO don't unwrap
+            .map(|value| {
+                let (key, value) = value.unwrap(); //TODO don't unwrap
+                let id = ClusterId::from(Uuid::from_slice(key.value()).unwrap()); //TODO don't unwrap
+
+                let value = serde_json::from_str::<ClusterDeployment>(&value.value()).unwrap(); //TODO don't unwrap
+                // let value = {
+                //     let SerializablePeerDescriptor { id, name, location, network, topology, executors } = value;
+                //     PeerDescriptor { id, name, location, network, topology, executors }
+                // };
+
+                (id, value)
+            })
+            .collect();
+
+        Ok(value)
     }
 }
+
+const CLUSTER_DEPLOYMENT_TABLE: TableDefinition<&[u8], String> = TableDefinition::new("cluster_deployment");
+
+//TODO SerializableClusterDeployment
